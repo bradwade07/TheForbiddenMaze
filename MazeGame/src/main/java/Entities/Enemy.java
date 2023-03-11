6eb90c8cc3329033b0e30bf9437532a421d0f7b3
Josh <jla797@sfu.ca>
2023-03-11 13:48:25 -0800

    Entity changes to make it use Point instead

--- MazeGame/src/main/java/Entities/Enemy.java
+++ MazeGame/src/main/java/Entities/Enemy.java
@@ -1,12 +1,23 @@
 package Entities;
 
+import Map.Point;
+import State.Game;
+
+/**
+ * A enemy class that creates the enemy entity
+ * Enemies randomly move towards the player
+ */
 public class Enemy extends Entity implements DynamicEntity{
-    Enemy(int x, int y){
-        super(x, y);
+
+    private final Game gameManager;
+
+    Enemy(Game game, Point location){
+        super(location);
+        this.gameManager = game;
     }
 
     @Override
-    public void move(int x, int y) {
+    public void move(Point location) {
 
     }
 }
