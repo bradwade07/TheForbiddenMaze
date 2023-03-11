fe3851d55ec169d030aed33a09af22420155e1e1
jjb19 <jjb19@sfu.ca>
2023-03-10 21:12:53 -0800

    Changed pom to work with javafx and added basic UI

--- MazeGame/src/main/java/Group2/App.java
+++ MazeGame/src/main/java/Group2/App.java
@@ -1,13 +1,32 @@
 package Group2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+           // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/main_menu.fxml"));
+           // Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+            //stage.setTitle("Hello!");
+            //stage.setScene(scene);
+            //stage.show();
+
+            Parent root = FXMLLoader.load(getClass().getResource("/main_menu.fxml"));
+            Scene scene = new Scene(root, 1920, 1080);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
