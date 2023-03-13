5410fdde99f89ff60f608471299f21f421ba2306
Josh <jla797@sfu.ca>
2023-03-12 23:15:53 -0700

    Roughh draft of how player movement will take in input

--- MazeGame/src/main/java/State/Game.java
+++ MazeGame/src/main/java/State/Game.java
@@ -9,31 +9,85 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Game class that handles all the gameplay
  */
-public class Game extends Application {
+public class Game {
 
     private Player player;
     private List<Enemy> enemies = new ArrayList<>();
     private Maze maze;
 
-    private int score;
+    private List<Reward> rewards = new ArrayList<>();
+
+    private final int rewardsToBeCollected = 5;
+    private int rewardsCollected = 0;
+
+    GameState gameState = GameState.game;
 
 
-    @Override
-    public void start(Stage stage) throws Exception {
+    public void runGame() {
+        boolean keepRunning = true;
+        do {
+            // get Player input
+            // move player
+            // move enemies
+            // update map?
+            // check collision
+            //
+            MoveDirection selection = getUserInput();
 
-        loadMaze();
-        loadPlayer();
-        placeEntitiesOnMap();
+            switch (selection) {
+                default -> {
+                    movePlayer(selection);
+                    // entities move
+//                    if (stateOfGame == GameState.WIN) {
+//                        displayWin();
+//                        keepRunning = false;
+//                    } else if (stateOfGame == GameState.LOSE) {
+//                        displayLoss();
+//                        keepRunning = false;
+//                    }
+                }
+            }
+            keepRunning = false;
+        } while (keepRunning);
 
+    }
+
+    // Input for UI, calls UI returns MoveDirection
+    private MoveDirection getUserInput() {
+        Scanner userInput = new Scanner(System.in);
+        MoveDirection move;
+        boolean formatValid;
+        char input;
+        System.out.print("Enter your move [WASD?]: ");
+        input = userInput.next().trim().charAt(0);
+        input = Character.toLowerCase(input);
+
+        switch (input) {
+            case ('w') -> move = MoveDirection.UP;
+            case ('a') -> move = MoveDirection.LEFT;
+            case ('s') -> move = MoveDirection.DOWN;
+            case ('d') -> move = MoveDirection.RIGHT;
+            default -> throw new RuntimeException("MazeGameManager movePlayer(): Invalid input");
+        }
+
+        return move;
+    }
+
+    public boolean hasPlayerLost(){
+        return (getCurrentScore() < 0);
+    }
+
+    public int getCurrentScore(){
+        return player.getScore();
     }
 
     private void placeEntitiesOnMap(){
         maze.setEntity(player);
-
     }
 
     private void loadPlayer(){
@@ -43,10 +97,6 @@ public class Game extends Application {
 
     private void loadMaze(){
         maze = Maze.generateRandomizedMaze();
-    }
-
-    public int getScore() {
-        return score;
     }
 
 
@@ -110,6 +160,16 @@ public class Game extends Application {
         }
 
     }
+
+
+    public int getRewardsToBeCollected() {
+        return rewardsToBeCollected;
+    }
+
+    public int getRewardsCollected() {
+        return rewardsCollected;
+    }
+
 }
 
 
