41d95077b2afe2b43d563d54b26b83d4ee884787
Josh <jla797@sfu.ca>
2023-03-13 20:52:38 -0700

    Added methods to check win conditions

--- MazeGame/src/main/java/State/Game.java
+++ MazeGame/src/main/java/State/Game.java
@@ -20,12 +20,11 @@ public class Game {
     private List<Enemy> enemies = new ArrayList<>();
     private Maze maze;
 
-    private List<Reward> rewards = new ArrayList<>();
 
     private final int rewardsToBeCollected = 5;
     private int rewardsCollected = 0;
 
-    GameState gameState = GameState.game;
+
 
 
     public void runGame() {
@@ -36,28 +35,30 @@ public class Game {
             // move enemies
             // update map?
             // check collision
-            //
+            // check win conditions
             MoveDirection selection = getUserInput();
 
             switch (selection) {
-                default -> {
+                case NONE:
+                    // no movement
+                    break;
+                default:
                     movePlayer(selection);
-                    // entities move
-//                    if (stateOfGame == GameState.WIN) {
-//                        displayWin();
-//                        keepRunning = false;
-//                    } else if (stateOfGame == GameState.LOSE) {
-//                        displayLoss();
-//                        keepRunning = false;
-//                    }
-                }
             }
+
+            // other entities move
             keepRunning = false;
         } while (keepRunning);
 
     }
 
-    // Input for UI, calls UI returns MoveDirection
+
+
+
+
+
+
+    // pass in an KeyEvent Handler and returns MoveDirection
     private MoveDirection getUserInput() {
         Scanner userInput = new Scanner(System.in);
         MoveDirection move;
@@ -72,17 +73,22 @@ public class Game {
             case ('a') -> move = MoveDirection.LEFT;
             case ('s') -> move = MoveDirection.DOWN;
             case ('d') -> move = MoveDirection.RIGHT;
-            default -> throw new RuntimeException("MazeGameManager movePlayer(): Invalid input");
+            default -> throw new RuntimeException("MazeGame movePlayer(): Invalid input");
         }
 
         return move;
     }
 
     public boolean hasPlayerLost(){
-        return (getCurrentScore() < 0);
+        return (getPlayerScore() < 0);
     }
 
-    public int getCurrentScore(){
+    public boolean hasPlayerWon(){
+        // TODO: and rewards are collected
+        return (player.getLocation().equals(maze.getExitCell()));
+    }
+
+    public int getPlayerScore(){
         return player.getScore();
     }
 
